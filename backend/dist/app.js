c90e601984ce388e50e8f371934ad99aeebd214f
kinhvi_pl <kinhvi0710@gmail.com>
2025-09-01 22:39:50 -0400

    Set up backend

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -1,16 +1,11 @@
 import express from "express";
 import cors from "cors";
-
-
 const app = express();
-
 // Middlewares
 app.use(cors());
 app.use(express.json());
-
-
 app.get("/", (req, res) => {
-  res.send("Hello, Concept Nexus!");
+    res.send("Hello, Concept Nexus!");
 });
-
 export default app;
+//# sourceMappingURL=app.js.map
